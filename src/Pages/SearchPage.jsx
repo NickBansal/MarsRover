44e059ddcd86be995938016c6c9efff58e50953a
Nick Bansal <https://github.com/NickBansal>
2019-01-09 21:59:03 +0000

    Axios call successful

--- src/Pages/SearchPage.jsx
+++ src/Pages/SearchPage.jsx
@@ -1,18 +1,21 @@
 import React, { Component } from 'react'
 import Navbar from '../Components/Navbar'
+import * as api from '../api'
 
 class SearchPage extends Component {
 
     state = {
-        searchTerm: ""
+        searchTerm: "",
+        searchItems: []
     }
 
     render() {
-        const { searchTerm } = this.state 
+        const { searchTerm } = this.state
+        const pageTitle = !searchTerm ? 'Please begin your search' : searchTerm
         return (
             <div>
-                <Navbar handleSubmit={this.handleSubmit}/>
-                <h1>{ searchTerm }</h1>
+                <Navbar handleSubmit={this.handleSubmit} />
+                <h1>{pageTitle}</h1>
             </div>
         )
     }
@@ -21,6 +24,20 @@ class SearchPage extends Component {
         this.setState({
             searchTerm
         })
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (prevState.searchTerm !== this.state.searchTerm) {
+            api.getPictures(this.state.searchTerm)
+            .then(items => {
+                this.setState({
+                    searchItems: items.collection.items
+                })
+            })
+            .catch(error => {
+                console.log(error, "<<<< ERROR")
+            })
+        }
     }
 }
 
--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 const API_URL = 'https://images-api.nasa.gov'
 
-export const getTopics = async () => {
-    const { data } = await axios.get(`${API_URL}/search`)
+export const getPictures = async (input) => {
+    const { data } = await axios.get(`${API_URL}/search?q=${input}`)
     return data
 }
