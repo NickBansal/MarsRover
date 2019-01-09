07a4cf7925e5e9009d6fc4176a62301e6ef975aa
Nick Bansal <https://github.com/NickBansal>
2019-01-09 21:13:37 +0000

    Landing page added with animations

--- src/App.js
+++ src/App.js
@@ -5,31 +5,17 @@ import LandingPage from './Pages/LandingPage'
 import AllSearchItems from './Pages/AllSearchItems'
 
 class App extends Component {
-
-  state = {
-    searchTerm: "ewgfweg"
-  }
-
   render() {
     return (
       <div className="App">
         <div className="Stars">
-          {/* <Navbar handleKeyPress={this.handleKeyPress}/> */}
           <Router>
             <LandingPage path='/'/>
-            <AllSearchItems 
-            searchTerm={this.state.searchTerm}
-            path='/search'/>
+            <AllSearchItems path='/search'/>
           </Router>
         </div>
       </div>
     );
-  }
-
-  handleKeyPress = (searchTerm) => {
-    this.setState({
-      searchTerm
-    })
   }
 }
 
