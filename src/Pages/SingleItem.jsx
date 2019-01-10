f261832906141ea0534ef70d458f75a9cc62d36c
Nick Bansal <https://github.com/NickBansal>
2019-01-10 16:08:01 +0000

    Styling started for single item

--- src/Pages/SingleItem.jsx
+++ src/Pages/SingleItem.jsx
@@ -1,36 +1,43 @@
 import React, { Component } from 'react'
 import * as api from '../api';
+import '../Stylesheets/SingleItem.css'
 
 class SingleItem extends Component {
-    
+
     state = {
         singleItem: {},
-        error: false, 
+        error: false,
         loading: true
     }
 
     render() {
-        const { loading } = this.state
+        const { loading, singleItem } = this.state
         return (
             <div>
-                { loading && <h1>Loading...</h1> }
-                { !loading && <h1>Single Item</h1> }
+                {loading && <h1>Loading...</h1>}
+
+                {!loading &&
+                <div className="SingleItem">
+                <h1>{ singleItem.data[0].title }</h1>
+                    <img src={singleItem.links[0].href}
+                        alt={singleItem.data[0].title} />
+                </div> }
             </div>
         )
     }
     componentDidMount() {
         api.getItems(this.props.id)
-        .then(data => {
-            this.setState({
-                singleItem: data.collection,
-                loading: false
+            .then(data => {
+                this.setState({
+                    singleItem: data.collection.items[0],
+                    loading: false
+                })
             })
-        })
-        .catch(error => {
-            this.setState({
-                error: true
+            .catch(error => {
+                this.setState({
+                    error: true
+                })
             })
-        })
     }
 }
 
