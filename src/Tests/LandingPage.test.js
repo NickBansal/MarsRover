c2a940db1a243de81489c50f4f49e8f3c10c61ca
Nick Bansal <https://github.com/NickBansal>
2019-01-11 16:50:37 +0000

    Landing Page tested

--- src/Tests/LandingPage.test.js
+++ src/Tests/LandingPage.test.js
@@ -4,12 +4,16 @@ import LandingPage from '../Pages/LandingPage';
 import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json'
 
-describe('<LandingPage />', () => {
+describe.only('<LandingPage />', () => {
     let wrapper
     beforeEach(() => {
         wrapper = shallow(<LandingPage />)
     })
     it('Component matches the snapshot', () => {
         expect(toJson(wrapper)).toMatchSnapshot()
+    })
+    it('Checking the amount of elements on the page', () => {
+        expect(wrapper.find('.LandingPage').exists()).toBeTruthy()
+        expect(wrapper.find('p').length).toBe(1)
     })
 })
