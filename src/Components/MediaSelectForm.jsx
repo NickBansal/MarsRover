51d8639bc077bfbbe9adc575d06e12792b0b7c1a
Nick Bansal <https://github.com/NickBansal>
2019-01-14 18:36:56 +0000

    labels attached to inputs

--- src/Components/MediaSelectForm.jsx
+++ src/Components/MediaSelectForm.jsx
@@ -5,15 +5,15 @@ const MediaSelectForm = ({ handleClick }) => {
     <form onClick={(e) => handleClick(e.target.value)}>
       <div className='RadioSelect'>
         <input id='image' type='radio' name='gender' value='image' defaultChecked />
-        <label value='image'>Image</label>
+        <label for='image'>Image</label>
       </div>
       <div className='RadioSelect'>
         <input id='video' type='radio' name='gender' value='video' />
-        <label value='video'>Video</label>
+        <label for='video'>Video</label>
       </div>
       <div className='RadioSelect'>
         <input id='audio' type='radio' name='gender' value='audio' />
-        <label value='audio'>Audio</label>
+        <label for='audio'>Audio</label>
       </div>
     </form>
   )
